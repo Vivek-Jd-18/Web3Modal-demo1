213de83fa8cb7a5cc5ea72328b66a7e12d7d9390
Vivek-Jd-18 <sergiokunaguero0009@gmail.com>
2022-10-06 18:03:59 +0530

    initial commit 1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Wallet from './components/Wallet';
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center">Hello</h1>
+      <div>
+        <Wallet/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
